b6c67b4f60667312811df5436a78231d701dea86
Nicholas Dan <nicholas.dan@usp.br>
2024-06-24 09:08:34 -0300

    Commit current progress

--- c-api/program.c
+++ c-api/program.c
@@ -1,10 +1,15 @@
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <inttypes.h>
 
+#include "src/adts/dyn-array.h"
+#include "src/index-file.h"
 #include "src/data-file.h"
 #include "src/utils/data-utils.h"
 #include "src/utils/cabecalho-utils.h"
+#include "src/utils/removed-list.h"
 
 const char* OP_NAME[] = {
     "create",
@@ -44,6 +49,51 @@ int main(int argc, char** argv){
     if(strcmp(operation, "help") == 0){
         help_print_op();
     }
+    else if(strcmp(operation, "create") == 0){
+        if(argc < 5){
+            fprintf(stderr, "Argumentos insuficientes!\n");
+            return EXIT_FAILURE;
+        }
+
+        char *csv_filename = argv[2];
+        char *data_filename = argv[3];
+        char *index_filename = argv[4];
+
+        FILE *csv_fptr, *data_fptr, *index_fptr;
+        if(!(csv_fptr = fopen(csv_filename, "r")) || 
+        !(data_fptr = fopen(data_filename, "wb+")) ||
+        !(index_fptr = fopen(index_filename, "wb"))){
+            perror("Erro ao abrir os arquivos");
+        }
+
+        if(create_data_file(csv_fptr, data_fptr) == -1){
+            perror("Erro durante a criação do arquivo de dados: ");
+            fclose(csv_fptr);
+            fclose(index_fptr);
+            fclose(data_fptr);
+            return EXIT_FAILURE;
+        }
+
+        DYN_ARRAY *index_arr;
+
+        if(!(index_arr = generate_index(data_fptr))){
+            perror("Erro durante a geração do índice");
+            return EXIT_FAILURE;
+        }
+
+        if(write_index(&index_arr, index_fptr) == -1){
+            perror("Erro durante a criação do arquivo de dados: ");
+            fclose(csv_fptr);
+            fclose(index_fptr);
+            fclose(data_fptr);
+            return EXIT_FAILURE;
+        }
+
+        fclose(index_fptr);
+        fclose(csv_fptr);
+        fclose(data_fptr);
+        return EXIT_SUCCESS;
+    }
     else if(strcmp(operation, "select") == 0){
         if(argc < 3){
             fprintf(stderr, "Argumentos insuficientes!\n");
@@ -58,11 +108,54 @@ int main(int argc, char** argv){
             return EXIT_FAILURE;
         }
 
-        JOGADOR j_query = read_query();
+        JOGADOR j_query = read_new_jogador();
 
         select_data(data_fptr, j_query);
 
         fclose(data_fptr);
+    }
+    else if(strcmp(operation, "edit") == 0){
+        if(argc < 4){
+            fprintf(stderr, "Argumentos insuficientes!\n");
+            return EXIT_FAILURE;
+        }
+
+        char* data_filename = argv[2];
+        char* index_filename = argv[3];
+        FILE *data_fptr, *index_fptr;
+
+        if(!(data_fptr = fopen(data_filename, "rb+")) || !check_status(data_fptr) ||
+        !(index_fptr = fopen(index_filename, "rb+")) || !check_status(index_fptr)){
+            perror("Erro ao abrir o arquivo: ");
+            return EXIT_FAILURE;
+        }
+
+        DYN_ARRAY *index_arr = load_index(index_fptr);
+        REM_LIST *rem_list = load_rem_list(data_fptr, BEST_FIT);
+
+        int32_t edit_id;
+        sscanf(argv[4], "%" PRId32, &edit_id);
+
+        // JOGADOR j_query = read_query();
+        JOGADOR new_j = read_new_jogador();
+
+        // select_data(data_fptr, j_query);
+
+        // Deleta usando somente o id
+        JOGADOR old_j_query = jNil;
+        old_j_query.id = edit_id;
+
+        int quant_rem;
+        delete_data(data_fptr, old_j_query, &quant_rem, &rem_list, &index_arr);
+
+        int quant_ins;
+        insert_data(data_fptr, new_j, &quant_ins, &rem_list, &index_arr);
+
+        write_rem_list(&rem_list, data_fptr);
+        write_index(&index_arr, index_fptr);
+
+        fclose(data_fptr);
+        fclose(index_fptr);
     }
     else if(strcmp(operation, "delete")){
         if(argc < 4){
--- socket-server.py
+++ socket-server.py
@@ -1,17 +1,123 @@
-# TESTE DA COMUNICAÇÃO ENTRE O PROGRAMA EM C E PYTHON
-
 import subprocess
 import csv
 import json
-import random
+import socket
+import threading
+import secrets
 
-socket_id = random.randint(0,100000)
+# Chamada da função create na API do arquivo
+def C_create(req, session_tok: str):
+    print(f"[CREATING FILE] Creating data-{session_tok}.bin")
+    p = subprocess.run(["c-api/bin/program", "create", f"c-api/{req["database"]}", f"data-{session_tok}.bin", f"indice-{session_tok}.bin"])
+    if p.returncode != 0:
+        print("Erro durante execução de criação do arquivo!")
+        return
+    print("Comando executado com sucesso")
 
-with open(f'select-response-{socket_id}.csv', 'w+') as csv_file, open(f'select-responce-{socket_id}.json', 'w') as json_file:
-    p1 = subprocess.run(["c-api/bin/program", "select", "c-api/test-files/binario9.bin"], input=b'1 nacionalidade SPAIN', stdout=csv_file)
-    if p1.returncode != 0:
-        exit(p1.returncode)
-    csv_file.flush()
-    csv_file.seek(0)
-    csv_reader = csv.DictReader(csv_file)
-    json.dump([row for row in csv_reader], json_file)
+# Chamada da função select na API do arquivo
+# tomar já que temos o envio de muitos dados
+def C_select(req, session_tok: str):
+    query = req["query"]
+    req_query = (f"{ query["id"] } "
+                f"{ query["idade"] if query["idade"] >= 0 else "NULO" } "
+                f"{ query["nomeJogador"] if query["nomeJogador"] else "NULO" } " 
+                f"{ query["nacionalidade"] if query["nacionalidade"] else "NULO" } "
+                f"{ query["nomeClube"] if query["nomeClube"] else "NULO" }"
+                )
+    with open(f'select-out-{session_tok}.csv', 'w+') as csv_file, open(f'select-out-{session_tok}.json', 'w') as json_file:
+        p = subprocess.run(["c-api/bin/program", "select", f"data-{session_tok}.bin"], input=req_query.encode('utf-8'), stdout=csv_file)
+        if p.returncode != 0:
+            print("Erro durante execução de criação do arquivo!")
+            return
+        print("Convertendo arquivo .csv para .json")
+        
+        csv_file.flush()
+        csv_file.seek(0)
+        csv_reader = csv.DictReader(csv_file)
+        json.dump([row for row in csv_reader], json_file)
+
+        print("Enviar arquivo para a interface")
+
+        print("Comando executado com sucesso")
+
+# Chamada da função edit na API do arquivo
+def C_edit(req, session_tok: str):
+    query = req["query"]
+    req_query = (f"{ query["id"] } "
+                f"{ query["idade"] if query["idade"] >= 0 else "NULO" } "
+                f"{ query["nomeJogador"] if query["nomeJogador"] else "NULO" } " 
+                f"{ query["nacionalidade"] if query["nacionalidade"] else "NULO" } "
+                f"{ query["nomeClube"] if query["nomeClube"] else "NULO" }"
+                )
+                
+    p = subprocess.run(["c-api/bin/program", "edit", f"data-{session_tok}.bin", f"indice-{session_tok}.bin", f"{query["id"]}"], input=req_query.encode('utf-8'))
+    if p.returncode != 0:
+        print("Erro durante execução de criação do arquivo!")
+        return
+
+    print("Comando executado com sucesso")
+
+# Chamada da função delete na API do arquivo
+def C_delete(req, session_tok: str):
+    print("hi")
+
+c_calls = {
+    "CREATE": C_create,
+    "SELECT": C_select,
+    "EDIT": C_edit,
+    "DELETE": C_delete,
+}
+
+def handle_client_thread(conn: socket.socket, addr, session_tok: str):
+    print(f"[NEW CONNECTION] {addr} connected (session_tok: {session_tok})")
+    
+    while True:
+        msg = conn.recv(1024)
+
+        if not msg:
+            break
+
+        try:
+            resp = json.loads(msg.decode('utf-8'))
+
+            op = resp["operation"]
+            if op == "CLOSE":
+                print(f"[CLOSE CONNECTION] Close connection with {addr}")
+                conn.close()
+                return
+            elif op in c_calls:
+                c_calls[op](resp, session_tok)
+            else:
+                print("Invalid operation!")
+
+        except json.JSONDecodeError as err:
+            print("Invalid JSON syntax: ", err)
+
+        except KeyError as err:
+            print("Invalid request; missing parameter: ", err)
+
+    print(f"[CLOSE CONNECTION] Close connection with {addr}")
+
+print("[STARTING] Starting server...")
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(('127.0.0.1', 5050))
+    s.listen()
+    while True:
+        conn, addr = s.accept()
+        session_tok = secrets.token_urlsafe(16)
+        thread = threading.Thread(target=handle_client_thread, args=(conn, addr, session_tok))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count()-1}, {conn.getsockname()}")
+
+
+
+# Criar o arquivo .csv para requests de select
+# with open(f'select-response-{socket_id}.csv', 'w+') as csv_file, open(f'select-responce-{socket_id}.json', 'w') as json_file:
+#     p1 = subprocess.run(["c-api/bin/program", "select", "c-api/test-files/binario9.bin"], input=b'1 nacionalidade SPAIN', stdout=csv_file)
+#     if p1.returncode != 0:
+#         exit(p1.returncode)
+#     csv_file.flush()
+#     csv_file.seek(0)
+#     csv_reader = csv.DictReader(csv_file)
+#     json.dump([row for row in csv_reader], json_file)
