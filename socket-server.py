f22552650b6bdcf441d4bed3ea34306480886ebd
Nicholas Dan <nicholas.dan@usp.br>
2024-06-24 10:48:02 -0300

    Send answers to request

--- socket-server.py
+++ socket-server.py
@@ -7,18 +7,20 @@ import secrets
 import os
 
 # Chamada da função create na API do arquivo
-def C_create(req, session_tok: str):
+def C_create(req, conn: socket.socket, session_tok: str):
     print(f"[CREATING FILE] Creating data-{session_tok}.bin and indice-{session_tok}.bin")
     p = subprocess.run(["c-api/bin/program", "create", f"c-api/{req["database"]}", f"data-{session_tok}.bin", f"indice-{session_tok}.bin"])
     
     if p.returncode != 0:
         print("Erro durante execução de criação do arquivo!")
+        conn.send(b'{"codigoRetorno": 0}') # Código de falha
         return
     print("Comando executado com sucesso")
+    conn.send(b'{"codigoRetorno": 1}') # Código de sucesso
 
 # Chamada da função select na API do arquivo
 # tomar já que temos o envio de muitos dados
-def C_select(req, session_tok: str):
+def C_select(req, conn: socket.socket, session_tok: str):
     query = req["query"]
     req_query = (f"{ query["id"] } "
                 f"{ query["idade"] if query["idade"] >= 0 else "NULO" } "
@@ -27,12 +29,14 @@ def C_select(req, session_tok: str):
                 f"{ query["nomeClube"] if query["nomeClube"] else "NULO" }"
                 )
             
-    with open(f'select-out-{session_tok}.csv', 'w+') as csv_file, open(f'select-out-{session_tok}.json', 'w') as json_file:
+    with open(f'select-out-{session_tok}.csv', 'w+') as csv_file, open(f'select-out-{session_tok}.json', 'w+') as json_file:
         p = subprocess.run(["c-api/bin/program", "select", f"data-{session_tok}.bin"], input=req_query.encode('utf-8'), stdout=csv_file)
         
         if p.returncode != 0:
-            print("Erro durante execução de criação do arquivo!")
+            print("Erro durante execução de seleção do arquivo!")
+            conn.send(b'{"codigoRetorno": 0}') # Código de falha
             return
+        
         print("Convertendo arquivo .csv para .json")
         
         csv_file.flush()
@@ -42,15 +46,20 @@ def C_select(req, session_tok: str):
 
         print("Enviar arquivo para a interface")
 
-        if os.path.exists(f"select-out-{session_tok}.csv"):
-            os.remove(f"select-out-{session_tok}.csv")
-        if os.path.exists(f"select-out-{session_tok}.json"):
-            os.remove(f"select-out-{session_tok}.json")
+        json_file.seek(0)
 
-        print("Comando executado com sucesso")
+        for line in json_file:
+            conn.send(line.encode('utf-8'))
+
+    if os.path.exists(f"select-out-{session_tok}.csv"):
+        os.remove(f"select-out-{session_tok}.csv")
+    if os.path.exists(f"select-out-{session_tok}.json"):
+        os.remove(f"select-out-{session_tok}.json")
+
+    print("Comando executado com sucesso")
 
 # Chamada da função edit na API do arquivo
-def C_edit(req, session_tok: str):
+def C_edit(req, conn, session_tok: str):
     newData = req["newData"]
     req_query = (f"{ newData["id"] } "
                 f"{ newData["idade"] if newData["idade"] >= 0 else "NULO" } "
@@ -63,19 +72,23 @@ def C_edit(req, session_tok: str):
     
     if p.returncode != 0:
         print("Erro durante execução de criação do arquivo!")
+        conn.send(b'{"codigoRetorno": 0}') # Código de falha
         return
 
     print("Comando executado com sucesso")
+    conn.send(b'{"codigoRetorno": 1}') # Código de sucesso
 
 # Chamada da função delete na API do arquivo
-def C_delete(req, session_tok: str):
+def C_delete(req, conn, session_tok: str):
     p = subprocess.run(["c-api/bin/program", "delete", f"data-{session_tok}.bin", f"indice-{session_tok}.bin", f"{req["id"]}"])
 
     if p.returncode != 0:
         print("Erro durante execução de criação do arquivo!")
+        conn.send(b'{"codigoRetorno": 0}') # Código de falha
         return
 
     print("Comando executado com sucesso")
+    conn.send(b'{"codigoRetorno": 1}') # Código de sucesso
 
 c_calls = {
     "CREATE": C_create,
@@ -108,7 +121,7 @@ def handle_client_thread(conn: socket.socket, addr, session_tok: str):
                 conn.close()
                 return
             elif op in c_calls:
-                c_calls[op](resp, session_tok)
+                c_calls[op](resp, conn, session_tok)
             else:
                 print("Invalid operation!")
 
@@ -123,7 +136,7 @@ def handle_client_thread(conn: socket.socket, addr, session_tok: str):
 print("[STARTING] Starting server...")
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind(('127.0.0.1', 5051))
+    s.bind(('127.0.0.1', 5050))
     s.listen()
     while True:
         conn, addr = s.accept()
